7b31ca626b0ebf4f67e5196dd3f0be516265f85e
Nathan Sandberg <nathansandberg@gmail.com>
2018-02-19 15:14:55 -0800

    untracked changes

--- js/pane.js
+++ js/pane.js
@@ -1,56 +1,59 @@
+function setup(){
+  createCanvas(800, 400);
+  // background(119, 244, 66);
 
+  // fill(204, 101, 192, 127);
+  // stroke(127, 63, 120);
 
-var rSlider, gSlider, bSlider;
-
-function setup() {
-  // create canvas
-  createCanvas(710, 400);
-  textSize(15);
-  noStroke();
-
-
-
-
-  // create sliders
-  rSlider = createSlider(0, 255, 100);
-  rSlider.position(20, 20);
-  rSlider.class('red');
-
-  gSlider = createSlider(0, 255, 0);
-  gSlider.position(20, 50);
-  gSlider.class('red');
-
-  bSlider = createSlider(0, 255, 255);
-  bSlider.position(20, 80);
-  bSlider.class('red');
-
+  // rect(40, 120, 120, 40);
+  // ellipse(600, 120, 60, 60);
 }
-function draw() {
-  var r = rSlider.value();
-  var g = gSlider.value();
-  var b = bSlider.value();
+var slider = createSlider(0, 255)
+var xPositions = [5, 354, 14, 25, 45, 66, 76, 134,150, 234, 300];
+var yPositions = [12,34, 23, 21, 21, 34, 30, 40, 23, 34, 21];
+var xPositionsB =[200, 125, 250, 175, 300, 225, 350, 375, 400];
+var yPositionsB =[50, 55, 54, 53, 48, 45, 57, 60, 67, 65, 80];
+
+draw = function() {
+  background(119, 244, 66);
+
+  for (var i = 0; i < xPositions.length; i++) {
+      noStroke();
+      fill(0, 200, 255);
+      ellipse(xPositions[i], yPositions[i], 10, 10);
+      stroke(5, 28, 11);
+      fill(66, 244, 107);
+      star(xPositionsB[i], yPositionsB[i], 2, 20, 6); 
+      // translate(width*.1, height*0.1);
+      // rotate(frameCount / 500);
+
+      yPositions[i] += 3;
+      yPositionsB[i] += 5;
+
+      if (yPositions[i] > 400){
+        yPositions[i] = 0;
+      
+      if (yPositionsB[i] > 400){
+        yPositionsB[i] = 0;
+      }
+
+  };
   
-  background(r, g, b);
-  // text("red", rSlider.x * 1 + rSlider.width, 35);
-  // text("green", gSlider.x * 1 + gSlider.width, 65);
-  // text("blue", bSlider.x * 1 + bSlider.width, 95);
-//   push();
-//   translate(width*0.2, height*0.5);
-//   rotate(frameCount / 10.0);
-//   star(15, 20, 2, 20, 4); 
-//   pop();
+      };
 
-// };
-// function star(x, y, radius1, radius2, npoints) {
-//   var angle = TWO_PI / npoints;
-//   var halfAngle = angle/2.0;
-//   beginShape();
-//   for (var a = 0; a < TWO_PI; a += angle) {
-//     var sx = x + cos(a) * radius2;
-//     var sy = y + sin(a) * radius2;
-//     vertex(sx, sy);
-//     sx = x + cos(a+halfAngle) * radius1;
-//     sy = y + sin(a+halfAngle) * radius1;
-//     vertex(sx, sy);
-//   }
-}
+
+};
+function star(x, y, radius1, radius2, npoints) {
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+};
