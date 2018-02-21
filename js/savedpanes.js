1538f3012cc845caf0662c07aaf8309dd90ba151
Mario Quintana <pyrros2000@gmail.com>
2018-02-21 10:57:58 -0800

    changed var to const on JS

--- js/savedpanes.js
+++ js/savedpanes.js
@@ -1,13 +1,13 @@
 
 
 function createRows() {
-    var tbody = document.getElementById("Panes");
-    var row = tbody.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Name";
-    cell2.innerHTML = "Feb. 2018";
-    cell3.innerHTML = "?";;
+    const tbody = document.getElementById('Panes');
+    const row = tbody.insertRow(0);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    cell1.innerHTML = 'Name';
+    cell2.innerHTML = 'Feb. 2018';
+    cell3.innerHTML = '?';;
 }
 createRows();
