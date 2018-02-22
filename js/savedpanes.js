6a85c57b16005e32963b7eaad25adb6739ebe06b
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-22 04:46:59 -0800

    I think it works

--- js/savedpanes.js
+++ js/savedpanes.js
@@ -19,15 +19,3 @@ if (localStorage.getItem('savedPanes')) {
         });
     }
 }
-// function createRows() {
-//     const tbody = document.getElementById('Panes');
-//     const row = tbody.insertRow(0);
-//     const cell1 = row.insertCell(0);
-//     const cell2 = row.insertCell(1);
-//     const cell3 = row.insertCell(2);
-//     cell1.innerHTML = 'Name';
-//     cell2.innerHTML = 'Feb. 2018';
-//     cell3.innerHTML = '?';;
-// }
-
-// createRows();
