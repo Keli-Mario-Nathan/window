816b04cdda33e75478726bae78de3652c365cec4
Mario Quintana <pyrros2000@gmail.com>
2018-02-23 09:28:57 -0800

    cleaned up some css

--- js/savedpanes.js
+++ js/savedpanes.js
@@ -9,9 +9,19 @@ if (localStorage.getItem('savedPanes')) {
         const cell1 = row.insertCell();
         const cell2 = row.insertCell();
         const cell3 = row.insertCell();
+        const cell4 = row.insertCell();
         cell1.textContent = saved[i].savedAs;
         cell2.textContent = saved[i].savedAt;
         cell3.textContent = 'Go!';
+        cell4.textContent = 'X';
+        cell4.addEventListener('click', function () {
+            event.preventDefault;
+            const result = confirm('Do you want to delete your pane?');
+            if(result == true){
+                document.getElementById('panes').deleteRow(0);
+                localStorage.removeItem('savedPanes');
+            }
+        });
         cell3.addEventListener('click', function() {
             event.preventDefault;
             localStorage.setItem('requestedPane', JSON.stringify(saved[i]));
