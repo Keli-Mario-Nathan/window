676991e979c3682b20dee5768697a58b22ac69c0
Mario Quintana <pyrros2000@gmail.com>
2018-02-22 16:12:01 -0800

    added delete option to table

--- js/savedpanes.js
+++ js/savedpanes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const tbody = document.getElementById('panes');
+// 
 
 if (localStorage.getItem('savedPanes')) {
     const saved = JSON.parse(localStorage.getItem('savedPanes'));
@@ -9,9 +10,17 @@ if (localStorage.getItem('savedPanes')) {
         const cell1 = row.insertCell();
         const cell2 = row.insertCell();
         const cell3 = row.insertCell();
+        const cell4 = row.insertCell();
         cell1.textContent = saved[i].savedAs;
         cell2.textContent = saved[i].savedAt;
         cell3.textContent = 'Go!';
+        cell4.textContent = 'Delete';
+        cell4.addEventListener('click', function () {
+            confirm('Do you want to delete your pane?');
+            event.preventDefault;
+            document.getElementById('panes').deleteRow(0);
+            localStorage.removeItem('requestedPane', JSON.stringify(saved[i]));
+        });
         cell3.addEventListener('click', function() {
             event.preventDefault;
             localStorage.setItem('requestedPane', JSON.stringify(saved[i]));
@@ -19,3 +28,4 @@ if (localStorage.getItem('savedPanes')) {
         });
     }
 }
+
